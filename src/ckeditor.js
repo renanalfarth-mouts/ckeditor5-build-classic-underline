caf8f2fe0c370efe3f4c4d12a92d24bcee4749a8
Renan Alfarth <renanalfarth@yahoo.com.br>
2020-06-25 06:20:08 -0700

    Allowing only indent block

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -20,7 +20,6 @@ import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
@@ -52,7 +51,6 @@ ClassicEditor.builtinPlugins = [
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
-	Indent,
 	IndentBlock,
 	Link,
 	List,
